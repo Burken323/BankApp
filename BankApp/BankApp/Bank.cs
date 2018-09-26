17a07a9068c481a5a210a83e7383a59595750288
Gabriel Jealmo <gabrieljealmo@gmail.com>
2018-09-26 09:08:21 +0200

    Added base for BankApp.

---
+++ BankApp/BankApp/Bank.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Bank
+    {
+        public Database db;
+        public Customers cs;
+
+        public void BankOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deposit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateNewCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ BankApp/BankApp/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Customer
+    {
+        public string Name;
+        public string organization;
+        public string organizationNumber;
+        public string zipCode;
+        public string address;
+
+        public Dictionary<string, string> cash;
+        
+        public Customer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
